f8cb95844bb28df74cd683bd1bb872fe858b9e12
Manas-tech <manastechnonjr@gmail.com>
2023-10-12 20:28:31 +0530

    Add files via upload

---
+++ stack-reverse.c
@@ -0,0 +1,35 @@
+#include <stdio.h>  
+#include <string.h>  
+  
+#define max 100  
+int top,stack[max];  
+  
+void push(char x){  
+  
+      // Push(Inserting Element in stack) operation  
+      if(top == max-1){  
+          printf("stack overflow");  
+      }  else {  
+          stack[++top]=x;  
+      }  
+  
+}  
+  
+void pop(){  
+    // Pop (Removing element from stack)  
+      printf("%c",stack[top--]);  
+}  
+  
+  
+int main()  
+{  
+   char str[]="HELLO WORLD";  
+   int len = strlen(str);  
+   int i;  
+  
+   for(i=0;i<len;i++)  
+        push(str[i]);  
+  
+   for(i=0;i<len;i++)  
+      pop();  
+} 
